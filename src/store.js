b11785422a68b35fa2e8217e5082003d3f8eb700
Hrishikesh Bolinjkar <bolinjkar.rishi@outlook.com>
2021-09-01 12:13:55 +0530

    updated initstate and reducer func to update state

--- src/store.js
+++ src/store.js
@@ -1,19 +1,34 @@
 import { createContext, useReducer } from "react";
-import { SET_ORDER_TYPE } from "./constants"
+import { CATEGORY_LIST_FAILURE, CATEGORY_LIST_REQUEST, CATEGORY_LIST_SUCCESS, SET_ORDER_TYPE } from "./constants"
 
 //initial state
 const initialState = {
+    categoryList: {
+        loading: true
+    },
     order:{
         orderType: 'eat in',
     },
 }
 
 //reducer function
-const Reducer = (state, action) => {
+const reducer = (state, action) => {
     switch(action.type){
         case SET_ORDER_TYPE:
             return {
                 ...state, order:{ ...state.order, orderType:action.payload }
+            };
+        case CATEGORY_LIST_REQUEST:
+            return {
+                ...state, categoryList:{ loading:true }
+            };
+        case CATEGORY_LIST_SUCCESS:
+            return {
+                ...state, categoryList:{ loading:false, categories:action.payload }
+            };
+        case CATEGORY_LIST_FAILURE: 
+            return {
+                ...state, categoryList:{ loading:false,  error:action.payload }
             };
         default: 
             return state;
@@ -23,7 +38,7 @@ const Reducer = (state, action) => {
 //store provider - creating the context
 export const Store = createContext();
 export const StoreProvider = (props) => {
-    const [state, dispatch] = useReducer(Reducer, initialState);
+    const [state, dispatch] = useReducer(reducer, initialState);
 
     return(
         <Store.Provider value={{state, dispatch}}>
