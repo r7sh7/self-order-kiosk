cdd609e07dc964be995c52b6dd305461995be2b9
Hrishikesh Bolinjkar <bolinjkar.rishi@outlook.com>
2021-09-09 23:07:57 +0530

    established connection with DB from express server

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const data = require('./data');
 
-const app = express();
 
+const app = express();
+const port = process.env.PORT || 5000;
+
+//api to get category data for listing the different categories to choose from 
 app.get('/api/categories', (req,res) => {
     res.send(data.categories);
 });
 
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => {
-    console.log(`serve at http://localhost:${port}`);
-})
+//configuring and connecting to mongoDB
+dotenv.config();
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser : true,
+    useUnifiedTopology: true
+}).then(result => {
+    app.listen(port, () => console.log(`serve at http://localhost:${port}`))
+}).catch(err => console.log(err));
