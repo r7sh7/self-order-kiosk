9954b4a48ba0ec21925dd23dfdcd7dc9d0d94a85
Hrishikesh Bolinjkar <bolinjkar.rishi@outlook.com>
2021-09-13 19:36:37 +0530

    constants + handling creatOrder action dispatch

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { createContext, useReducer } from "react";
-import { CATEGORY_LIST_FAILURE, CATEGORY_LIST_REQUEST, CATEGORY_LIST_SUCCESS, PRODUCT_LIST_FAILURE, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, ORDER_SET_TYPE, ORDER_ADD_ITEM, ORDER_REMOVE_ITEM, ORDER_CLEAR, PAYMENT_SET_TYPE } from "./constants"
+import { CATEGORY_LIST_FAILURE, CATEGORY_LIST_REQUEST, CATEGORY_LIST_SUCCESS, PRODUCT_LIST_FAILURE, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, ORDER_SET_TYPE, ORDER_ADD_ITEM, ORDER_REMOVE_ITEM, ORDER_CLEAR, PAYMENT_SET_TYPE, ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_CREATE_FAILURE } from "./constants"
 
 //initial state
 const initialState = {
@@ -10,6 +10,7 @@ const initialState = {
         orderItems: [],
         paymentType: 'Pay here'
     },
+    orderCreate: { loading: true }
 }
 
 //reducer function
@@ -17,33 +18,40 @@ const reducer = (state, action) => {
     switch(action.type){
         case CATEGORY_LIST_REQUEST:
             return {
-                ...state, categoryList:{ loading:true }
+                ...state, 
+                categoryList:{ loading:true }
             };
         case CATEGORY_LIST_SUCCESS:
             return {
-                ...state, categoryList:{ loading:false, categories:action.payload }
+                ...state, 
+                categoryList:{ loading:false, categories:action.payload }
             };
         case CATEGORY_LIST_FAILURE: 
             return {
-                ...state, categoryList:{ loading:false,  error:action.payload }
+                ...state, 
+                categoryList:{ loading:false,  error:action.payload }
             };
 
         case PRODUCT_LIST_REQUEST:
                 return {
-                    ...state, productList:{ loading:true }
+                    ...state, 
+                    productList:{ loading:true }
                 };
         case PRODUCT_LIST_SUCCESS:
                 return {
-                    ...state, productList:{ loading:false, products:action.payload }
+                    ...state, 
+                    productList:{ loading:false, products:action.payload }
                 };
         case PRODUCT_LIST_FAILURE: 
                 return {
-                    ...state, productList:{ loading:false,  error:action.payload }
+                    ...state, 
+                    productList:{ loading:false,  error:action.payload }
                 };
 
         case ORDER_SET_TYPE:
                 return {
-                    ...state, order:{ ...state.order, orderType:action.payload }
+                    ...state, 
+                    order:{ ...state.order, orderType:action.payload }
                 };
         case ORDER_ADD_ITEM: {
             const item = action.payload;
@@ -57,7 +65,8 @@ const reducer = (state, action) => {
             const total = Math.round((itemsPrice + tax) * 100) / 100;
 
             return{
-                ...state, order: {...state.order, orderItems, itemsCount, tax, total }
+                ...state, 
+                order: {...state.order, orderItems, itemsCount, tax, total }
             };
         }
         case ORDER_REMOVE_ITEM: {
@@ -72,17 +81,36 @@ const reducer = (state, action) => {
 
 
             return{
-                ...state, order: {...state.order, orderItems, itemsCount, tax, total}    
+                ...state, 
+                order: {...state.order, orderItems, itemsCount, tax, total}    
             };
         }   
         case ORDER_CLEAR: 
             return{
-                ...state, order: { ...state.order, orderItems: [], tax: 0, total: 0, itemsCount: 0 },
+                ...state, 
+                order: { ...state.order, orderItems: [], tax: 0, total: 0, itemsCount: 0 },
             };
 
         case PAYMENT_SET_TYPE: 
             return{
-                ...state, order:{ ...state.order, paymentType: action.payload } 
+                ...state, 
+                order:{ ...state.order, paymentType: action.payload } 
+            };
+
+        case ORDER_CREATE_REQUEST:
+            return{
+                ...state, 
+                orderCreate:{ loading: true }
+            };
+        case ORDER_CREATE_SUCCESS: 
+            return {
+                ...state, 
+                orderCreate: { loading: false, newOrder: action.payload }
+            };
+        case ORDER_CREATE_FAILURE:
+            return{
+                ...state, 
+                orderCreate: { loading: false, error: action.payload }
             };
 
         default: 
