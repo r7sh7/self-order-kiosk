743305c03236ae212c5081e7ee2b7a8a38cd9d64
Hrishikesh Bolinjkar <bolinjkar.rishi@gmail.com>
2021-11-17 19:37:00 +0530

    deploy only server to heroku

--- server.js
+++ server.js
@@ -1,43 +1,46 @@
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const data = require('./data');
-const orderRoutes = require('./src/routes/orderRoutes');
-const productRoutes = require('./src/routes/productRoutes');
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const data = require("./data");
+const orderRoutes = require("./src/routes/orderRoutes");
+const productRoutes = require("./src/routes/productRoutes");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-//api to get category data for listing the different categories to choose from 
-app.get('/api/categories', (req,res) => {
-    res.send(data.categories);
+//api to get category data for listing the different categories to choose from
+app.get("/api/categories", (req, res) => {
+  res.send(data.categories);
 });
 
 //configuring and connecting to mongoDB
 dotenv.config();
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser : true,
-    useUnifiedTopology: true
-}).then(result => {
-    app.listen(port, () => console.log(`serve at http://localhost:${port}`))
-}).catch(err => console.log(err));
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) => {
+    app.listen(port, () => console.log(`serve at http://localhost:${port}`));
+  })
+  .catch((err) => console.log(err));
 
 //making the build folder files static for FE
-app.use(express.static(path.join(__dirname, '/build')));
+// app.use(express.static(path.join(__dirname, '/build')));
 
-//serving the build folder 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/build/index.html'));
+//serving the build folder
+app.get("/", (req, res) => {
+  // res.sendFile(path.join(__dirname, '/build/index.html'));
+  res.send("Server is running properly");
 });
 
-
-//middleware for parsing req body 
+//middleware for parsing req body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //using productRoutes
-app.use('/api/products', productRoutes);
+app.use("/api/products", productRoutes);
 
 //using orderRoutes
-app.use('/api/orders', orderRoutes);
+app.use("/api/orders", orderRoutes);
--- src/actions.js
+++ src/actions.js
@@ -1,86 +1,106 @@
-import axios from "axios";
-import { CATEGORY_LIST_FAILURE, CATEGORY_LIST_REQUEST, CATEGORY_LIST_SUCCESS, ORDER_ADD_ITEM, ORDER_CLEAR, ORDER_CREATE_FAILURE, ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_LIST_FAILURE, ORDER_LIST_REQUEST, ORDER_LIST_SUCCESS, ORDER_QUEUE_FAILURE, ORDER_QUEUE_REQUEST, ORDER_QUEUE_SUCCESS, ORDER_REMOVE_ITEM, ORDER_SET_TYPE, PAYMENT_SET_TYPE, PRODUCT_LIST_FAILURE, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, SCREEN_SET_WIDTH, } from "./constants";
+import request from "./api";
+import {
+  CATEGORY_LIST_FAILURE,
+  CATEGORY_LIST_REQUEST,
+  CATEGORY_LIST_SUCCESS,
+  ORDER_ADD_ITEM,
+  ORDER_CLEAR,
+  ORDER_CREATE_FAILURE,
+  ORDER_CREATE_REQUEST,
+  ORDER_CREATE_SUCCESS,
+  ORDER_LIST_FAILURE,
+  ORDER_LIST_REQUEST,
+  ORDER_LIST_SUCCESS,
+  ORDER_QUEUE_FAILURE,
+  ORDER_QUEUE_REQUEST,
+  ORDER_QUEUE_SUCCESS,
+  ORDER_REMOVE_ITEM,
+  ORDER_SET_TYPE,
+  PAYMENT_SET_TYPE,
+  PRODUCT_LIST_FAILURE,
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  SCREEN_SET_WIDTH,
+} from "./constants";
 
 export const setPaymentType = (dispatch, paymentType) => {
-    return dispatch({
-        type: PAYMENT_SET_TYPE,
-        payload: paymentType
-    })
+  return dispatch({
+    type: PAYMENT_SET_TYPE,
+    payload: paymentType,
+  });
 };
 
-
 export const setOrderType = (dispatch, orderType) => {
-       return dispatch({
-            type: ORDER_SET_TYPE,
-            payload: orderType
-        });
+  return dispatch({
+    type: ORDER_SET_TYPE,
+    payload: orderType,
+  });
 };
 
 export const listCategories = async (dispatch) => {
-    dispatch({type: CATEGORY_LIST_REQUEST});
-    try{
-        const { data } = await axios.get('/api/categories');
-        dispatch({type: CATEGORY_LIST_SUCCESS, payload: data});
-    }catch(error){
-        dispatch({type: CATEGORY_LIST_FAILURE, payload: error.message});
-    }
+  dispatch({ type: CATEGORY_LIST_REQUEST });
+  try {
+    const { data } = await request.get("/api/categories");
+    dispatch({ type: CATEGORY_LIST_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({ type: CATEGORY_LIST_FAILURE, payload: error.message });
+  }
 };
 
-export const listProducts = async(dispatch, category) => {
-    dispatch({type: PRODUCT_LIST_REQUEST});
-    try{
-        const { data } = await axios.get(`/api/products?category=${category}`);
-        return dispatch({type: PRODUCT_LIST_SUCCESS, payload: data});
-    }catch(error){
-        return dispatch({type: PRODUCT_LIST_FAILURE, payload: error.message});
-    }
+export const listProducts = async (dispatch, category) => {
+  dispatch({ type: PRODUCT_LIST_REQUEST });
+  try {
+    const { data } = await request.get(`/api/products?category=${category}`);
+    return dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
+  } catch (error) {
+    return dispatch({ type: PRODUCT_LIST_FAILURE, payload: error.message });
+  }
 };
 
 export const addToOrder = (dispatch, item) => {
-    return dispatch({type: ORDER_ADD_ITEM, payload: item});
+  return dispatch({ type: ORDER_ADD_ITEM, payload: item });
 };
 
 export const removeFromOrder = (dispatch, item) => {
-    return dispatch({type: ORDER_REMOVE_ITEM, payload: item});
+  return dispatch({ type: ORDER_REMOVE_ITEM, payload: item });
 };
 
-
 export const clearOrder = (dispatch) => {
-    return dispatch({ type: ORDER_CLEAR });
+  return dispatch({ type: ORDER_CLEAR });
 };
 
 export const createOrder = async (dispatch, order) => {
-    dispatch({ type: ORDER_CREATE_REQUEST });
-    try{
-        // axios.post('/api/orders', order)
-        //     .then((data) => dispatch({ type: ORDER_CREATE_SUCCESS, payload: data })); 
-        // dispatch({ type: ORDER_CLEAR });
-        const { data } = await axios.post('/api/orders', order);
-        dispatch({ type: ORDER_CREATE_SUCCESS, payload: data });
-        dispatch({ type: ORDER_CLEAR });
-    }catch(error){
-        console.log(error);
-        dispatch({ type: ORDER_CREATE_FAILURE, payload: error.message });
-    }
+  dispatch({ type: ORDER_CREATE_REQUEST });
+  try {
+    // axios.post('/api/orders', order)
+    //     .then((data) => dispatch({ type: ORDER_CREATE_SUCCESS, payload: data }));
+    // dispatch({ type: ORDER_CLEAR });
+    const { data } = await request.post("/api/orders", order);
+    dispatch({ type: ORDER_CREATE_SUCCESS, payload: data });
+    dispatch({ type: ORDER_CLEAR });
+  } catch (error) {
+    console.log(error);
+    dispatch({ type: ORDER_CREATE_FAILURE, payload: error.message });
+  }
 };
 
 export const listOrders = async (dispatch) => {
-    dispatch({ type: SCREEN_SET_WIDTH })
-    dispatch({ type: ORDER_LIST_REQUEST });
-    try{
-        const { data } = await axios.get('/api/orders');
-        return dispatch({ type: ORDER_LIST_SUCCESS, payload: data});
-    }catch(error){
-        return dispatch({ type: ORDER_LIST_FAILURE, payload: error.message });
-    }
+  dispatch({ type: SCREEN_SET_WIDTH });
+  dispatch({ type: ORDER_LIST_REQUEST });
+  try {
+    const { data } = await request.get("/api/orders");
+    return dispatch({ type: ORDER_LIST_SUCCESS, payload: data });
+  } catch (error) {
+    return dispatch({ type: ORDER_LIST_FAILURE, payload: error.message });
+  }
 };
 
 export const listQueue = async (dispatch) => {
-    dispatch({ type: ORDER_QUEUE_REQUEST});
-    try{
-        const { data } = await axios.get('/api/orders/queue');
-        dispatch({ type: ORDER_QUEUE_SUCCESS, payload: data });
-    }catch(err){
-        dispatch({ type: ORDER_QUEUE_FAILURE, payload: err.message });
-    }
-}
+  dispatch({ type: ORDER_QUEUE_REQUEST });
+  try {
+    const { data } = await request.get("/api/orders/queue");
+    dispatch({ type: ORDER_QUEUE_SUCCESS, payload: data });
+  } catch (err) {
+    dispatch({ type: ORDER_QUEUE_FAILURE, payload: err.message });
+  }
+};
